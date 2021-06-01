1caca9e9f6a73e24e67165f19b07517d24bff3be
adharamonzon <adharamonzon@gmail.com>
2021-06-01 15:56:12 +0200

    change styles in header

--- src/components/Projects/ProjectList.jsx
+++ src/components/Projects/ProjectList.jsx
@@ -1,19 +1,19 @@
 import { Link } from 'react-router-dom';
 const ProjectList = (props) => {
-  if (props.projectList.length > 0 ) {
-    const project = props.projectList.map((item)=> {
+  if (props.projectList.length > 0) {
+    const project = props.projectList.map((item) => {
       return (
-        <Link to={`/project/${item.id}`} className="projectList--link">
-        <li className="projectList--item" key={item.id}>
-           <h1 className="projectList--item__title">{item.title}</h1>
-           <p className="projectList--item__description">{item.description}</p>
-        </li>
+        <Link to={`/project/${item.id}`} className='projectList--link'>
+          <li className='projectList--item' key={item.id}>
+            <h1 className='projectList--item__title'>{item.title}</h1>
+            <p className='projectList--item__description'>{item.description}</p>
+          </li>
         </Link>
-      )
-    })
-    return <ul className="projectList">{project}</ul>
+      );
+    });
+    return <ul className='projectList'>{project}</ul>;
   } else {
-    return <p className='projectList-error'>No hay todavía ningún proyecto</p>
+    return <p className='projectList--error'>No hay todavía ningún proyecto</p>;
   }
-}
+};
 export default ProjectList;
