70bfde595dab06ca7b96f0001e6f4bc63af07983
adharamonzon <adharamonzon@gmail.com>
2021-06-07 13:45:12 +0200

    add hooks to the state

--- src/App.js
+++ src/App.js
@@ -7,21 +7,15 @@ import Header from './components/header/Header';
 import Home from './components/main/Home';
 import Project from './components/Projects/Project';
 
-const projects = [];
-
 function App() {
-  const [data, setData] = useState({
-    title: '',
-    description: '',
-    id: '',
-  });
+  let [title, setTitle] = useState({ title: '' });
+  let [id, setId] = useState({ id: '' });
+  let [description, setDescription] = useState({ description: '' });
 
-  const handleNewProject = (ev) => {
-    setData({
-      ...data,
-      [ev.target.name]: ev.currentTarget.value,
-    });
-    console.log(data);
+  const handleNewProject = (props) => {
+    setTitle(props.title);
+    setId(props.id);
+    setDescription(props.description);
   };
 
   return (
@@ -30,7 +24,7 @@ function App() {
         <Header />
         <Switch>
           <Route path='/' exact>
-            <Home handleNewProject={handleNewProject} />
+            <Home handleNewProject={handleNewProject} title={title} id={id} description={description} />
           </Route>
           <Route path='/proyect/:id'>
             <Project />
--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,38 +1,53 @@
 
 import ProjectList from '../Projects/ProjectList.jsx';
 
+const Home = (props) => {
+console.log(props);
+let data = {title:'', id:'', description: ''};
+let projects = []
+projects.title = props.title;
+projects.id = props.id;
+projects.description = props.description;
+console.log(projects);
+const handleChangeTitle = ev => {
+  data.title = ev.currentTarget.value;
+  return data.title
+}
+const handleChangeId = (ev) => {
+  data.id = ev.currentTarget.value;
+  return data.id
+}
+const handleChangeDescription = (ev) => {
+  data.description = ev.currentTarget.value;
+  return console.log(data.description)
+}
+  const handleNewProject = () => {
+   props.handleNewProject(data);
 
-
-const Home = () => {
-
-  const handleNewProject = (ev) => {
-   console.log(ev.target.value);
   }
-  const addNewProject = (ev) => {
-   /*  console.log(ev, 'me han clickado'); */
-  }
+ 
   return(
     <main className='main'>
     <form action="post"  className="form">
          <div className="form--header">
            <h3>Crea un tablero nuevo</h3>
-            <input className="form--header__title" required name='title' onChange={handleNewProject} type="text" placeholder=" Nombre del proyecto *" />
+            <input className="form--header__title" required name='title' onChange={handleChangeTitle} type="text" placeholder=" Nombre del proyecto *" />
          </div>
          <div className="form--body">
-          <input className="form--body__id" required name='id' type='text' placeholder=" Añade un id único para tu proyecto *" onChange={handleNewProject} />
-          <textarea className="form--body__description" name='description' onChange={handleNewProject} type="text" placeholder=" Introduce una breve descripción de tu proyecto" />
+          <input className="form--body__id" required name='id' type='text' placeholder=" Añade un id único para tu proyecto *" onChange={handleChangeId} />
+          <textarea className="form--body__description" name='description' onChange={handleChangeDescription} type="text" placeholder=" Introduce una breve descripción de tu proyecto" />
          </div>
          <div className="form--footer">
-          <button type="button" className="form--footer__button"  onClick={addNewProject} >Crear tablero</button>
+          <button type="button" className="form--footer__button"  onClick={handleNewProject} >Crear tablero</button>
         </div> 
     </form>
     <section className='yourProjects'>
     <h3 className='yourProjects--title'>Tus proyectos</h3>
-      <div>{/* 
-        <ProjectList /> */}
+      <div>
+        <ProjectList projectList={projects}/>
         </div>
-  </section>
-</main>
+    </section>
+    </main>
     )} 
       
 export default Home;
